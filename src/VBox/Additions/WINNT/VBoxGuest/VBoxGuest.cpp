fd7c1657788b40fadb17d9233e2c8e3ee8f1026d
? <?>
2010-08-06 17:15:52 +0400

    imported VirtualBox-3.2.8-OSE.tar.bz2
    http://download.virtualbox.org/virtualbox/3.2.8/VirtualBox-3.2.8-OSE.tar.bz2

--- src/VBox/Additions/WINNT/VBoxGuest/VBoxGuest.cpp
+++ src/VBox/Additions/WINNT/VBoxGuest/VBoxGuest.cpp
@@ -259,6 +259,9 @@ static NTSTATUS VBoxGuestAddDevice(PDRIVER_OBJECT pDrvObj, PDEVICE_OBJECT pDevOb
     hlpRegisterBugCheckCallback(pDevExt); /* ignore failure! */
 #endif
 
+    /* VBoxGuestPower is pageable; ensure we are not called at elevated IRQL */
+    deviceObject->Flags |= DO_POWER_PAGABLE;
+
     /* Driver is ready now. */
     deviceObject->Flags &= ~DO_DEVICE_INITIALIZING;
 
@@ -829,7 +832,7 @@ static uint32_t guestAtomicBitsTestAndClear(void *pu32Bits, uint32_t u32Mask)
  */
 NTSTATUS VBoxGuestDeviceControl(PDEVICE_OBJECT pDevObj, PIRP pIrp)
 {
-    dprintf(("VBoxGuest::VBoxGuestDeviceControl\n"));
+    //dprintf(("VBoxGuest::VBoxGuestDeviceControl\n"));
 
     NTSTATUS Status = STATUS_SUCCESS;
 
@@ -869,7 +872,7 @@ NTSTATUS VBoxGuestDeviceControl(PDEVICE_OBJECT pDevObj, PIRP pIrp)
              * array of counters for each event, event mask is computed, each
              * time a wait event is arrived.
              */
-            dprintf(("VBoxGuest::VBoxGuestDeviceControl: VBOXGUEST_IOCTL_WAITEVENT\n"));
+            //dprintf(("VBoxGuest::VBoxGuestDeviceControl: VBOXGUEST_IOCTL_WAITEVENT\n"));
 
             if (pStack->Parameters.DeviceIoControl.OutputBufferLength < sizeof(VBoxGuestWaitEventInfo))
             {
@@ -923,7 +926,7 @@ NTSTATUS VBoxGuestDeviceControl(PDEVICE_OBJECT pDevObj, PIRP pIrp)
 
                 rc = KeWaitForSingleObject (&pDevExt->keventNotification, Executive /** @todo UserRequest? */,
                                             KernelMode, TRUE, &timeout);
-                dprintf(("VBOXGUEST_IOCTL_WAITEVENT: Wait returned %d -> event %x\n", rc, eventInfo->u32EventFlagsOut));
+                //dprintf(("VBOXGUEST_IOCTL_WAITEVENT: Wait returned %d -> event %x\n", rc, eventInfo->u32EventFlagsOut));
 
                 if (!fTimeout && rc == STATUS_TIMEOUT)
                     continue;
@@ -1426,8 +1429,7 @@ NTSTATUS VBoxGuestDeviceControl(PDEVICE_OBJECT pDevObj, PIRP pIrp)
 
     IoCompleteRequest(pIrp, IO_NO_INCREMENT);
 
-    dprintf(("VBoxGuest::VBoxGuestDeviceControl: returned cbOut=%d rc=%#x\n", cbOut, Status));
-
+    //dprintf(("VBoxGuest::VBoxGuestDeviceControl: returned cbOut=%d rc=%#x\n", cbOut, Status));
     return Status;
 }
 
